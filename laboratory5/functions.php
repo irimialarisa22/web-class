142fcd2db717331ef42e0f5e5e3892917ac11618
irimialarisa22 <ieae0070@scs.ubbcluj.ro>
2021-05-23 11:41:24 +0300

    finish PHP lab

--- laboratory5/functions.php
+++ laboratory5/functions.php
@@ -14,6 +14,8 @@ function pdo_connect_mysql() {
 }
 // Template header
 function template_header($title) {
+// Get the amount of items in the shopping cart, this will be displayed in the header.
+$num_items_in_cart = isset($_SESSION['cart']) ? count($_SESSION['cart']) : 0;
 echo <<<EOT
 <!DOCTYPE html>
 <html>
@@ -32,6 +34,9 @@ echo <<<EOT
                     <a href="index.php?page=products">Products</a>
                 </nav>
                 <div class="link-icons">
+                    <a href="index.php?page=cart">
+						<i class="fas fa-shopping-cart"></i><span>$num_items_in_cart</span>
+					</a>
                 </div>
             </div>
         </header>
